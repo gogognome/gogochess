a69834287e849b5f039f4d13fcfd33ed029140a4
Sander Kooijmans <sander@gogognome.nl>
2018-01-17 22:55:54 +0100

    Implemented equals, hashCode and toString.

--- src/main/java/nl/gogognome/gogochess/game/BoardMutation.java
+++ src/main/java/nl/gogognome/gogochess/game/BoardMutation.java
@@ -11,7 +11,7 @@ public class BoardMutation {
 	private final Square square;
 	private final Mutation mutation;
 
-	public BoardMutation(PlayerPiece playerPiece, Square square, Mutation mutation) {
+	BoardMutation(PlayerPiece playerPiece, Square square, Mutation mutation) {
 		if (playerPiece == null || square == null || mutation == null) {
 			throw new IllegalArgumentException("Arguments must not be null");
 		}
@@ -31,5 +31,26 @@ public class BoardMutation {
 
 	public Mutation getMutation() {
 		return mutation;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null || !(obj.getClass().equals(this.getClass()))) {
+			return false;
+		}
+		BoardMutation that = (BoardMutation) obj;
+		return this.playerPiece == that.playerPiece
+				&& this.square.equals(that.square)
+				&& this.mutation == that.mutation;
+	}
+
+	@Override
+	public int hashCode() {
+		return playerPiece.hashCode() * 23 + square.hashCode() * 131 * mutation.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return mutation + " " + playerPiece + "@" + square;
 	}
 }
