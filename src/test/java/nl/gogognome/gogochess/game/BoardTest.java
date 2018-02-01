3d4fbd6fe59528fea07fa06d6c1a8253bf91d9fa
Sander Kooijmans <sander@gogognome.nl>
2018-02-01 20:29:42 +0100

    Moved pawn tests to separate class.

--- src/test/java/nl/gogognome/gogochess/game/BoardTest.java
+++ src/test/java/nl/gogognome/gogochess/game/BoardTest.java
@@ -1,29 +1,14 @@
 package nl.gogognome.gogochess.game;
 
-import static java.util.Arrays.*;
 import static java.util.Collections.*;
-import static java.util.stream.Collectors.*;
 import static nl.gogognome.gogochess.game.Board.*;
 import static nl.gogognome.gogochess.game.BoardMutation.Mutation.*;
 import static nl.gogognome.gogochess.game.Player.*;
+import static nl.gogognome.gogochess.game.Squares.*;
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.*;
 import org.junit.jupiter.api.*;
 
 class BoardTest {
-
-	private static final Square A2 = new Square("A2");
-	private static final Square A3 = new Square("A3");
-	private static final Square A4 = new Square("A4");
-	private static final Square A5 = new Square("A5");
-	private static final Square A6 = new Square("A6");
-	private static final Square A7 = new Square("A7");
-	private static final Square A8 = new Square("A8");
-	private static final Square B1 = new Square("B1");
-	private static final Square B2 = new Square("B2");
-	private static final Square B5 = new Square("B5");
-	private static final Square B7 = new Square("B7");
-	private static final Square B8 = new Square("B8");
 
 	private Board board = new Board();
 
@@ -86,150 +71,9 @@ class BoardTest {
 	}
 
 	@Test
-	public void validMovesForEmptyBoard() {
+	void validMovesForEmptyBoard() {
 		assertEquals(emptyList(), board.validMoves(WHITE));
 		assertEquals(emptyList(), board.validMoves(BLACK));
-	}
-
-	@Test
-	void validMovesForPawnAtInitialPosition() {
-		Move setup = new Move("setup", null,
-				new BoardMutation(WHITE_PAWN, A2, ADD));
-		board.process(setup);
-
-		List<Move> moves = board.validMoves(WHITE);
-
-		assertEquals("[a2-a3, a2-a4]", moves.toString());
-		assertEquals(singleton(setup), moves.stream().map(Move::getPrecedingMove).collect(toSet()));
-		assertEquals(
-				asList(
-						asList(new BoardMutation(WHITE_PAWN, A2, REMOVE), new BoardMutation(WHITE_PAWN, A3, ADD)),
-						asList(new BoardMutation(WHITE_PAWN, A2, REMOVE), new BoardMutation(WHITE_PAWN, A4, ADD))),
-				moves.stream().map(Move::getBoardMutations).collect(toList()));
-	}
-
-	@Test
-	void validMovesForPawnThatCannotMoveForward() {
-		Move setup = new Move("setup", null,
-				new BoardMutation(WHITE_PAWN, A2, ADD),
-				new BoardMutation(BLACK_PAWN, A3, ADD));
-		board.process(setup);
-
-		List<Move> moves = board.validMoves(WHITE);
-
-		assertEquals("[]", moves.toString());
-		assertEquals(emptySet(), moves.stream().map(Move::getPrecedingMove).collect(toSet()));
-		assertEquals(
-				emptyList(),
-				moves.stream().map(Move::getBoardMutations).collect(toList()));
-	}
-
-	@Test
-	void validMovesForPawnAtInitialPositionThatCannotMoveForwardOneStepButNotTwo() {
-		Move setup = new Move("setup", null,
-				new BoardMutation(WHITE_PAWN, A2, ADD),
-				new BoardMutation(BLACK_PAWN, A4, ADD));
-		board.process(setup);
-
-		List<Move> moves = board.validMoves(WHITE);
-
-		assertEquals("[a2-a3]", moves.toString());
-		assertEquals(singleton(setup), moves.stream().map(Move::getPrecedingMove).collect(toSet()));
-		assertEquals(
-				asList(
-						asList(new BoardMutation(WHITE_PAWN, A2, REMOVE), new BoardMutation(WHITE_PAWN, A3, ADD))),
-				moves.stream().map(Move::getBoardMutations).collect(toList()));
-	}
-
-	@Test
-	void validMovesForPawnThatCanCaptureAnotherPiece() {
-		Move setup = new Move("setup", null,
-				new BoardMutation(WHITE_PAWN, B2, ADD),
-				new BoardMutation(BLACK_PAWN, A3, ADD));
-		board.process(setup);
-
-		List<Move> moves = board.validMoves(WHITE);
-
-		assertTrue(moves.toString().contains("b2xa3"), "actual moves: " + moves);
-		List<List<BoardMutation>> actualMoves = moves.stream().map(Move::getBoardMutations).collect(toList());
-		assertTrue(actualMoves.contains(asList(
-				new BoardMutation(WHITE_PAWN, B2, REMOVE),
-				new BoardMutation(BLACK_PAWN, A3, REMOVE),
-				new BoardMutation(WHITE_PAWN, A3, ADD))),
-				actualMoves.toString());
-	}
-
-	@Test
-	void validMovesForPawnThatCanCaptureAnotherPawnEnPassant() {
-		Move setup = new Move("setup", null,
-				new BoardMutation(WHITE_PAWN, B5, ADD),
-				new BoardMutation(BLACK_PAWN, A7, ADD));
-		board.process(setup);
-		Move blackPawnMoves2 = new Move("a7-a5", setup,
-				new BoardMutation(BLACK_PAWN, A7, REMOVE),
-				new BoardMutation(BLACK_PAWN, A5, ADD));
-		board.process(blackPawnMoves2);
-
-		List<Move> moves = board.validMoves(WHITE);
-
-		assertTrue(moves.toString().contains("b5xa6"), "actual moves: " + moves);
-		List<List<BoardMutation>> actualMoves = moves.stream().map(Move::getBoardMutations).collect(toList());
-		assertTrue(actualMoves.contains(asList(
-				new BoardMutation(WHITE_PAWN, B5, REMOVE),
-				new BoardMutation(BLACK_PAWN, A5, REMOVE),
-				new BoardMutation(WHITE_PAWN, A6, ADD))),
-				actualMoves.toString());
-	}
-
-	@Test
-	void validMovesForPawnThatCannotCaptureAnotherPawnEnPassantBeacuseOtherPawnDidNotMoveTwoStepsInPreviousMove() {
-		Move setup = new Move("setup", null,
-				new BoardMutation(WHITE_PAWN, B5, ADD),
-				new BoardMutation(BLACK_PAWN, A6, ADD));
-		board.process(setup);
-		Move blackPawnMoves2 = new Move("a6-a5", setup,
-				new BoardMutation(BLACK_PAWN, A6, REMOVE),
-				new BoardMutation(BLACK_PAWN, A5, ADD));
-		board.process(blackPawnMoves2);
-
-		List<Move> moves = board.validMoves(WHITE);
-
-		assertFalse(moves.toString().contains("b5xa6"), "actual moves: " + moves);
-	}
-
-	@Test
-	void validMovesForPawnThatCanMoveAndPromote() {
-		Move setup = new Move("setup", null,
-				new BoardMutation(WHITE_PAWN, B7, ADD));
-		board.process(setup);
-
-		List<Move> moves = board.validMoves(WHITE);
-
-		assertTrue(moves.toString().contains("b7-b8(N), b7-b8(B), b7-b8(R), b7-b8(Q)"), "actual moves: " + moves);
-		List<List<BoardMutation>> actualMoves = moves.stream().map(Move::getBoardMutations).collect(toList());
-		assertTrue(actualMoves.contains(asList(WHITE_PAWN.removeFrom(B7), WHITE_KNIGHT.addTo(B8))), actualMoves.toString());
-		assertTrue(actualMoves.contains(asList(WHITE_PAWN.removeFrom(B7), WHITE_BISHOP.addTo(B8))), actualMoves.toString());
-		assertTrue(actualMoves.contains(asList(WHITE_PAWN.removeFrom(B7), WHITE_ROOK.addTo(B8))), actualMoves.toString());
-		assertTrue(actualMoves.contains(asList(WHITE_PAWN.removeFrom(B7), WHITE_QUEEN.addTo(B8))), actualMoves.toString());
-		assertFalse(actualMoves.contains(asList(WHITE_PAWN.removeFrom(B7), WHITE_PAWN.addTo(B8))), actualMoves.toString());
-	}
-
-	@Test
-	void validMovesForPawnThatCanCaptureAnotherPieceAndPromote() {
-		Move setup = new Move("setup", null,
-				new BoardMutation(WHITE_PAWN, B7, ADD),
-				new BoardMutation(BLACK_ROOK, A8, ADD));
-		board.process(setup);
-
-		List<Move> moves = board.validMoves(WHITE);
-
-		assertTrue(moves.toString().contains("b7xRa8(N), b7xRa8(B), b7xRa8(R), b7xRa8(Q)"), "actual moves: " + moves);
-		List<List<BoardMutation>> actualMoves = moves.stream().map(Move::getBoardMutations).collect(toList());
-		assertTrue(actualMoves.contains(asList(WHITE_PAWN.removeFrom(B7), BLACK_ROOK.removeFrom(A8), WHITE_KNIGHT.addTo(A8))), actualMoves.toString());
-		assertTrue(actualMoves.contains(asList(WHITE_PAWN.removeFrom(B7), BLACK_ROOK.removeFrom(A8), WHITE_BISHOP.addTo(A8))), actualMoves.toString());
-		assertTrue(actualMoves.contains(asList(WHITE_PAWN.removeFrom(B7), BLACK_ROOK.removeFrom(A8), WHITE_ROOK.addTo(A8))), actualMoves.toString());
-		assertTrue(actualMoves.contains(asList(WHITE_PAWN.removeFrom(B7), BLACK_ROOK.removeFrom(A8), WHITE_QUEEN.addTo(A8))), actualMoves.toString());
-		assertFalse(actualMoves.contains(asList(WHITE_PAWN.removeFrom(B7), BLACK_ROOK.removeFrom(A8), WHITE_PAWN.addTo(A8))), actualMoves.toString());
 	}
 
 }
