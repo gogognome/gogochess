6283f8c9e67c6e2d346ceeb487741a9c7867674e
Sander Kooijmans <sander@gogognome.nl>
2018-02-01 20:30:01 +0100

    Moved square constants to separate class.

---
+++ src/test/java/nl/gogognome/gogochess/game/Squares.java
@@ -0,0 +1,18 @@
+package nl.gogognome.gogochess.game;
+
+public class Squares {
+
+	public static final Square A2 = new Square("A2");
+	public static final Square A3 = new Square("A3");
+	public static final Square A4 = new Square("A4");
+	public static final Square A5 = new Square("A5");
+	public static final Square A6 = new Square("A6");
+	public static final Square A7 = new Square("A7");
+	public static final Square A8 = new Square("A8");
+	public static final Square B1 = new Square("B1");
+	public static final Square B2 = new Square("B2");
+	public static final Square B5 = new Square("B5");
+	public static final Square B7 = new Square("B7");
+	public static final Square B8 = new Square("B8");
+
+}
