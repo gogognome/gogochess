162a7aa865fbaecd5c22dd419f5a6e917df6ddf7
Sander Kooijmans <sander@gogognome.nl>
2018-04-05 20:59:04 +0200

    Fixed typos.

--- src/test/java/nl/gogognome/gogochess/logic/ai/MiniMaxAlphaBetaArtificialIntelligenceTest.java
+++ src/test/java/nl/gogognome/gogochess/logic/ai/MiniMaxAlphaBetaArtificialIntelligenceTest.java
@@ -4,16 +4,16 @@ import org.junit.jupiter.api.*;
 
 //maxDepth: 3
 //evaluating 46561 positions took 23.735353679 s (1961.672896460571 positions/s
-//generatin191589 positions took 23.735353679 s (8071.88309013948 positions/s
+//generating 191589 positions took 23.735353679 s (8071.88309013948 positions/s
 //maxDepth: 5
 //evaluating 18083 positions took 37.205764346 s (486.02683798764923 positions/s
-//generatin482175 positions took 37.205764346 s (12959.685373372493 positions/s
+//generating 482175 positions took 37.205764346 s (12959.685373372493 positions/s
 //maxDepth: 5
 //evaluating 15192 positions took 1.49804405 s (10141.22381781764 positions/s
-//generatin40555 positions took 1.49804405 s (27071.967610031224 positions/s
+//generating 40555 positions took 1.49804405 s (27071.967610031224 positions/s
 //maxDepth: 6
 //evaluating 1822357 positions took 48.30310163 s (37727.53588287535 positions/s
-//generatin2155884 positions took 48.30310163 s (44632.41339063468 positions/s
+//generating 2155884 positions took 48.30310163 s (44632.41339063468 positions/s
 class MiniMaxAlphaBetaArtificialIntelligenceTest extends ArtificialIntelligenceTest {
 
 	private int maxDepth = 2;
