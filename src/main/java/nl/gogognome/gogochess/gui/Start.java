b3600f4cad5f48f1d03ef26cee393663b41d21c1
Sander Kooijmans <sander@gogognome.nl>
2019-02-22 08:45:41 +0100

    Process moves passed in as command line arguments

--- src/main/java/nl/gogognome/gogochess/gui/Start.java
+++ src/main/java/nl/gogognome/gogochess/gui/Start.java
@@ -4,10 +4,14 @@ import static java.awt.BorderLayout.*;
 import static javax.swing.WindowConstants.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.net.URL;
+import java.net.*;
+import java.util.List;
 import javax.swing.*;
 import com.google.inject.*;
 import nl.gogognome.gogochess.juice.Module;
+import nl.gogognome.gogochess.logic.*;
+import nl.gogognome.gogochess.logic.ai.*;
+import nl.gogognome.gogochess.logic.movenotation.*;
 
 public class Start {
 
@@ -33,7 +37,13 @@ public class Start {
 
 		setIcon(frame);
 
-		SwingUtilities.invokeLater(gamePresentationModel::init);
+		SwingUtilities.invokeLater(() -> {
+			gamePresentationModel.init();
+			List<Move> initialMoves = new BoardSetup(new ReverseAlgebraicNotation()).parseMoves(args);
+			for (Move move : initialMoves) {
+				gamePresentationModel.onMove(move);
+			}
+		});
 	}
 
 	private static void setIcon(JFrame frame) {
