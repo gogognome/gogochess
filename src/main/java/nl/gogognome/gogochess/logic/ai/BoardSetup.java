4f76d75ef54c3d51f224e8920f3933c59854bb5f
Sander Kooijmans <sander@gogognome.nl>
2019-02-22 08:43:49 +0100

    Add method to parse moves without processing them

--- src/main/java/nl/gogognome/gogochess/logic/ai/BoardSetup.java
+++ src/main/java/nl/gogognome/gogochess/logic/ai/BoardSetup.java
@@ -1,21 +1,31 @@
 package nl.gogognome.gogochess.logic.ai;
 
-import nl.gogognome.gogochess.logic.Board;
-import nl.gogognome.gogochess.logic.Move;
-import nl.gogognome.gogochess.logic.movenotation.MoveNotation;
-
-import java.util.List;
+import java.util.*;
+import nl.gogognome.gogochess.logic.*;
+import nl.gogognome.gogochess.logic.movenotation.*;
 
 /**
  * Sets up a board in the initial position and replays moves.
  * The moves are passed in as strings in the configured notation.
  */
-class BoardSetup {
+public class BoardSetup {
 
     private final MoveNotation moveNotation;
 
-    BoardSetup(MoveNotation moveNotation) {
+    public BoardSetup(MoveNotation moveNotation) {
         this.moveNotation = moveNotation;
+    }
+
+    public List<Move> parseMoves(String... moves) {
+        Board board = new Board();
+        board.initBoard();
+        List<Move> parsedMoves = new ArrayList<>();
+        for (String move : moves) {
+            Move parsedMove = parseMove(board, move);
+            parsedMoves.add(parsedMove);
+            board.process(parsedMove);
+        }
+        return parsedMoves;
     }
 
     void setupBoard(Board board, String... moves) {
@@ -26,13 +36,22 @@ class BoardSetup {
     }
 
     private void processMove(Board board, String move) {
+        Move parsedMove = parseMove(board, move);
+        board.process(parsedMove);
+    }
+
+    private Move parseMove(Board board, String move) {
+        Move parsedMove = null;
         List<Move> validMoves = board.currentPlayer().validMoves(board);
         for (Move validMove : validMoves) {
             if (move.equals(moveNotation.format(validMove))) {
-                board.process(validMove);
-                return;
+                parsedMove = validMove;
+                break;
             }
         }
-        throw new IllegalArgumentException("The move " + move + " is invalid");
+        if (parsedMove == null) {
+            throw new IllegalArgumentException("The move " + move + " is invalid");
+        }
+        return parsedMove;
     }
 }
